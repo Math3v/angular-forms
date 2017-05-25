b14c562cb13f8f5b79ddad2bdc0889945ca3f970
Matej Minarik <mato.minarik@azet.sk>
2017-05-25 14:45:06 +0200

    Add reactive form tests

--- src/app/user-forms/reactive-form/reactive-form.component.spec.ts
+++ src/app/user-forms/reactive-form/reactive-form.component.spec.ts
@@ -1,3 +1,5 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReactiveFormComponent } from './reactive-form.component';
@@ -8,7 +10,12 @@ describe('ReactiveFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReactiveFormComponent ]
+      imports: [
+        ReactiveFormsModule
+      ],
+      declarations: [
+        ReactiveFormComponent
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +28,45 @@ describe('ReactiveFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('builds form correctly', () => {
+    let form: FormGroup = component.buildForm();
+
+    expect( form.get('name') ).toBeTruthy();
+    expect( form.get('address').get('street') ).toBeTruthy();
+    expect( form.get('address').get('number') ).toBeTruthy();
+    expect( form.get('passwords').get('password') ).toBeTruthy();
+    expect( form.get('passwords').get('passwordConfirmation') ).toBeTruthy();
+  });
+
+  it('validates passwords', () => {
+    component.userForm = component.buildForm();
+
+    let passwordControl = component.userForm.get('passwords').get('password');
+    let passwordConfirmationControl = component.userForm.get('passwords').get('passwordConfirmation');
+
+    passwordControl.setValue('asd456AD');
+    passwordControl.markAsDirty();
+
+
+    passwordConfirmationControl.setValue('asd456AE');
+    passwordConfirmationControl.markAsDirty();
+
+    component.validateControls( component.userForm.controls );
+    expect( component.formErrors['passwords'] ).toMatch('must match');
+
+    passwordControl.setValue('foobar');
+    passwordConfirmationControl.setValue('foobar');
+
+    component.validateControls( component.userForm.controls );
+    expect( component.formErrors['password'] ).toMatch('uppercase and lowercase');
+    expect( component.formErrors['passwordConfirmation'] ).toMatch('uppercase and lowercase');
+
+    passwordControl.setValue('asd456AD');
+    passwordConfirmationControl.setValue('asd456AD');
+
+    component.validateControls( component.userForm.controls );
+    expect( Object.keys(component.formErrors).length ).toBe(0);
   });
 });

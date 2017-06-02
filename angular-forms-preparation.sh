e867bcae05055308128ab7f89a43dbf9aa446d88
Matej Minarik <mato.minarik@azet.sk>
2017-06-02 16:27:13 +0200

    Add preparation script

---
+++ angular-forms-preparation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPO='git@github.com:Math3v/angular-forms.git'
+
+# Check the Node and NPM versions
+node --version
+npm --version
+
+# Install Angular-CLI
+npm install -g @angular/cli
+
+# Clone the starters and reference repositories
+git clone -b template-starter $REPO template-starter-angular-forms
+git clone -b reactive-starter $REPO reactive-starter-angular-forms
+git clone $REPO
+
+cd template-starter-angular-forms && npm install
+cd ../reactive-starter-angular-forms && npm install
